4b4b1b2c9c08e8cb98429abf42924a8ff7db540f
Ryo Katsuse <ryokatsu0719@gmail.com>
2022-05-16 02:34:25 +0900

    feat: header add css

--- src/App.svelte
+++ src/App.svelte
@@ -1,32 +1,27 @@
 <script lang="ts">
-  export let name: string;
+  import Header from './components/Header.svelte';
 </script>
 
-<main>
-  <h1>Hello {name}!</h1>
-  <p>
-    Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
-    how to build Svelte apps.
-  </p>
-</main>
+<div class="app">
+  <Header />
+  <main>
+    <!-- add checkboxes, Graph -->
+  </main>
+</div>
 
 <style>
-  main {
+  .app {
     text-align: center;
-    padding: 1em;
     max-width: 240px;
     margin: 0 auto;
   }
 
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4em;
-    font-weight: 100;
+  main {
+    max-width: calc(600px + 10%);
+    margin: 0px auto;
   }
-
   @media (min-width: 640px) {
-    main {
+    .app {
       max-width: none;
     }
   }
---
+++ src/components/Header.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+</script>
+
+<header class="header">
+  <h1 class="header-h1">総人口推移</h1>
+</header>
+
+<style>
+  .header {
+    width: 100%;
+    background: rgb(49, 151, 149);
+    height: 80px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .header-h1 {
+    font-size: 24px;
+    font-weight: 700;
+    color: #fff;
+  }
+
+  @media (min-width: 640px) {
+  }
+</style>
