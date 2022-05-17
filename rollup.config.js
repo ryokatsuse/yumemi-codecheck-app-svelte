21e11d377d27f35e1a3a42079a8adc17bf029da2
Ryo Katsuse <ryokatsu0719@gmail.com>
2022-05-17 23:50:35 +0900

    fix:envを追加

--- rollup.config.js
+++ rollup.config.js
@@ -7,6 +7,9 @@ import sveltePreprocess from 'svelte-preprocess'
 import typescript from '@rollup/plugin-typescript'
 import css from 'rollup-plugin-css-only'
 
+import dotenv from 'dotenv'
+dotenv.config()
+
 const production = !process.env.ROLLUP_WATCH
 
 function serve () {
@@ -40,7 +43,10 @@ export default {
     },
     plugins: [
         svelte({
-            preprocess: sveltePreprocess({ sourceMap: !production }),
+            preprocess: sveltePreprocess({
+                sourceMap: !production,
+                replace: [['process.env.API_KEY', JSON.stringify(process.env.API_KEY)]],
+            }),
             compilerOptions: {
                 // enable run-time checks when not in production
                 dev: !production,
